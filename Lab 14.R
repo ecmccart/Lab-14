6aa323098a7c2085494e36bfc364a815171ee6f0
ecmccart <ecmccart@purdue.edu>
2025-05-11 03:12:12 -0400

    Update Lab 14.R

--- Lab 14.R
+++ Lab 14.R
@@ -122,10 +122,9 @@ reg3 <- lm(corn_price ~ SUR + P2006 + SUR:P2006, data = WASDE)
 summary(reg3)
 
 # Collect the residuals from the last regression, create a time series of the errors with a one-year lag of the error, then regress the error terms on the lagged error terms
-error <- ts(resid(reg3), start=1973, end=2019, frequency=1)   # the ts() function tells R to set the errors as a time-series 
-lag_error <- lag(error, -1)                                   # the lag() function creates a one-period lag of the error term
-error <- cbind(error, lag_error)                              # cbind() binds the specified vectors together as columns to create a new data frame
-
+error <- ts(resid(reg3), start=1973, end=2019, frequency=1)  
+lag_error <- lag(error, -1)                                  
+error <- cbind(error, lag_error)                             
 reg4 <- lm(error ~ lag_error, data=error)
 
 summary(reg4)
